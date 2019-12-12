bb5a628844dbda96064f31c5916ae3165362071d
Jonas Otto <jottosmail@gmail.com>
2019-12-12 14:12:52 +0100

    adds fetch

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,5 +15,7 @@ git checkout "$SRC_BRANCH"
 echo "Adding target remote"
 git remote add target "$DESTINATION_REPO"
 
+git fetch --all
+
 echo "Pushing changes $(git rev-parse --abbrev-ref HEAD):$DESTINATION_BRANCH"
 git push target "$(git rev-parse --abbrev-ref HEAD):$DESTINATION_BRANCH"
